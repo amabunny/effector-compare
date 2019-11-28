9075f9a12bacc276422fcf9bc98e2d9f872fcb96
Sergey <iamgreyy@gmail.com>
2019-11-28 12:34:59 +0500

    Move build files to docs

---
+++ docs/precache-manifest.cb03a739b15ec178fc2d127d5ea2d500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f1f90d507b856d59492be4cfbcb019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d66862864ee7cfa6ae",
+    "url": "/static/css/2.68c05dbd.chunk.css"
+  },
+  {
+    "revision": "7ae3a1df1c33781291cc",
+    "url": "/static/css/main.0409772e.chunk.css"
+  },
+  {
+    "revision": "00d66862864ee7cfa6ae",
+    "url": "/static/js/2.8cfe8db4.chunk.js"
+  },
+  {
+    "revision": "7ae3a1df1c33781291cc",
+    "url": "/static/js/main.23d3d409.chunk.js"
+  },
+  {
+    "revision": "7543503f859c64b58e19",
+    "url": "/static/js/runtime-main.17a9a596.js"
+  }
+]);

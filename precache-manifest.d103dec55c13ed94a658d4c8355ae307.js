6cb6a590a569a68807989fe318ee1cdb7f27e70e
Sergey <iamgreyy@gmail.com>
2019-11-29 01:36:38 +0500

    Updates

---
+++ precache-manifest.d103dec55c13ed94a658d4c8355ae307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558be1f83d807a22034c9975ea834e9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9723cb89635b302b586e",
+    "url": "/static/css/2.68c05dbd.chunk.css"
+  },
+  {
+    "revision": "2fa550d5932a5834f6fd",
+    "url": "/static/css/main.74827970.chunk.css"
+  },
+  {
+    "revision": "9723cb89635b302b586e",
+    "url": "/static/js/2.0534c536.chunk.js"
+  },
+  {
+    "revision": "2fa550d5932a5834f6fd",
+    "url": "/static/js/main.4e28e208.chunk.js"
+  },
+  {
+    "revision": "7543503f859c64b58e19",
+    "url": "/static/js/runtime-main.17a9a596.js"
+  }
+]);
